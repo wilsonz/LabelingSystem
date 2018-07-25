7cd86a00e52385336ec10d0e11c037adc0f01255
euphratica z <wilsonxyz@foxmail.com>
2018-07-25 10:52:39 +0800

    update blog.py

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -49,3 +49,59 @@ def create():
             return redirect(url_for('blog.index'))
 
     return render_template('blog/create.html')
+
+
+# Both /update/ and /delete/ views will need to fetch a post by id
+#     and check if the author matches the logged in user.
+# To avoid duplicating code, we write a func to get the post and call it
+#     from each view.
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'SELECT p.id, title, body, create, author_id, username \
+         FROM post p JOIN user u ON p.author_id = u.id \
+         WHERE p.id = ?',
+         (id,)
+    ).fetchone()
+
+    if post is None:
+        # abort    raise a special exception that returns an HTTP status code.
+        abort(404, "Post id {0} doesn't exist.".format(id))
+    # check_author     arg is defined so that the func can be used to get
+    #                  a post without checking the author.
+    if check_author and post['author_id'] != g.user['id']:
+        # 403      'Forbidden'
+        # 401      'Unauthorized'
+        abort(403)
+
+    return post
+
+
+#
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+# unlike the views u've written so far, the /update/ func takes an arg, id.
+# That corresponds to the <int:id> in the route.
+def update(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET title = ?. body = ?\
+                 WHERE id = ?',
+                (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/update.html', post=post)
